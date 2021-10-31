37feefd1d9cf665ef70a2ac9ad8b14155d91baaf
Jannik Wempe <jannik.wempe@iteratec.com>
2021-10-31 18:00:28 +0100

    chore: add tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,16 @@
+import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		target: '#svelte',
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
 };
 
 export default config;
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+const config = {
+	mode: 'jit',
+	purge: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {},
+	},
+
+	plugins: [
+    require('@tailwindcss/forms')
+  ],
+};
+
+module.exports = config;
