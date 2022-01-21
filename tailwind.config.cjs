6d821336b3880607dc381573ca05dd6417d3acac
Jannik Wempe <jannik.wempe@iteratec.com>
2022-01-21 19:09:18 +0100

    chore: update dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,8 @@
 const config = {
-	mode: 'jit',
-	purge: ['./src/**/*.{html,js,svelte,ts}'],
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {},
 	},
-
 	plugins: [
     require('@tailwindcss/forms')
   ],
