54b203c9f9845f6e18b904a0fe99f1c77903713f
Jannik Wempe <jannik.wempe@iteratec.com>
2022-02-21 21:32:36 +0100

    chore: mount svelte directly in body

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,6 @@
 import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		// target: '#svelte',
-		// this is no longer required and should be removed
-	},
-
 	preprocess: [
 		preprocess({
 			postcss: true,
